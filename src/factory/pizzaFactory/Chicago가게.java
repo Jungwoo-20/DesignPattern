f0ffe5f5bf6a10ead8b4c3f0f8d070f1a07ce590
Jungwoo-20 <jungwoo7250@naver.com>
2021-12-10 12:30:00 +0900

    create() 생성자 수정

--- src/factory/pizzaFactory/Chicago가게.java
+++ src/factory/pizzaFactory/Chicago가게.java
@@ -5,8 +5,8 @@ public class Chicago가게 extends PizzaStore {
     // Factory Method: 자신의 피자를 생성함:
     Pizza create(String 유형) {
         Pizza pizza = null;
-        if (유형.equals("치즈피자")) pizza = new NY치즈피자();
-        else if (유형.equals("야채피자")) pizza = new NY야채피자();
+        if (유형.equals("치즈피자")) pizza = new Chicago치즈피자();
+        else if (유형.equals("야채피자")) pizza = new Chicago야채피자();
         //else if (유형.equals("감자피자")) return new NY감자피자();
         return pizza;
     }
